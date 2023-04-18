af11a197fa821c8bea48bc32aa62912e3050fa93
Pavel Marianov <paul@Pavels-MacBook-Pro.local>
2023-04-18 20:52:29 +0100

    Add counter functionality

---
+++ src/background/index.ts
@@ -0,0 +1,44 @@
+import { runtime, storage } from 'webextension-polyfill'
+import { getCurrentTab } from '../helpers/tabs'
+
+type Message = {
+  from: string
+  to: string
+  action: string
+}
+
+// async function getCurrentTab() {
+//   const list = await tabs.query({ active: true, currentWindow: true })
+//
+//   return list[0]
+// }
+
+async function incrementStoredValue(tabId: string) {
+  const data = await storage.local.get(tabId)
+  const currentValue = data?.[tabId] ?? 0
+
+  return storage.local.set({ [tabId]: currentValue + 1 })
+}
+
+export async function init() {
+  await storage.local.clear()
+
+  runtime.onMessage.addListener(async (message: Message) => {
+    if (message.to === 'background') {
+      console.log('background handled: ', message.action)
+
+      const tab = await getCurrentTab()
+      const tabId = tab.id
+
+      if (tabId) {
+        return incrementStoredValue(tabId.toString())
+      }
+    }
+  })
+}
+
+runtime.onInstalled.addListener(() => {
+  init().then(() => {
+    console.log('[background] loaded ')
+  })
+})
